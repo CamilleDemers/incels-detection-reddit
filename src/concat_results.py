005fe380b00b7a63206e6155023da233f58ac980
CamilleDemers <camille.demers@umontreal.ca>
2024-12-05 13:47:39 -0500

    reroulé exp sbert + concaténé résultats

--- src/concat_results.py
+++ src/concat_results.py
@@ -1,13 +1,10 @@
 import os
 import pandas as pd 
 
-folder = '../results'
-files = [os.path.join(folder, file) for file in os.listdir(folder) if 'training' in file]
+folder = 'results'
+files = [os.path.join(folder, file) for file in os.listdir(folder) if 'sbert_results_train' in file]
 
 df = pd.concat([pd.read_csv(file) for file in files])
 df.sort_values(
-    by='f1-score', ascending=False
-    ).to_csv('../results/results_training.csv', index=False)
-
-top_20 = df.sort_values(by='f1-score', ascending=False)[:20]
-df.sort_values(by='f1-score', ascending=False)
+    by='rank_test_f1_macro', ascending=False
+    ).to_csv('results/results_training_sbert.csv', index=False)
